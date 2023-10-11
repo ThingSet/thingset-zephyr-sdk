55c18b08102038c1daf94ecbae53ff7fc9c7885b
Gareth Potter <gareth.potter@brillpower.com>
2023-10-11 15:22:16 +0100

    CAN: check before trying to extend buffer

--- src/can.c
+++ src/can.c
@@ -186,6 +186,13 @@ static void thingset_can_report_rx_cb(const struct device *dev, struct can_frame
                 (struct thingset_can_rx_context *)buffer->user_data;
             if (context->seq++ == frame->data[0]) {
                 int size = can_dlc_to_bytes(frame->dlc) - 1;
+                if (buffer->len + size > buffer->size) {
+                    LOG_WRN("Discarding packetised message from 0x%X for data ID 0x%X as it is too "
+                            "large.",
+                            source_addr, data_id);
+                    thingset_can_free_rx_buf(buffer);
+                    return;
+                }
                 uint8_t *buf = net_buf_add(buffer, size);
                 int pos = 0;
                 bool finished =
