22a2ccf3e100acd90287a1e90c3dbb1e4b09cc3f
Martin Jäger <martin@libre.solar>
2023-10-18 18:04:54 +0200

    Update clang-format to v15

--- src/can.c
+++ src/can.c
@@ -253,7 +253,8 @@ static void thingset_can_report_tx_handler(struct k_work *work)
             uint8_t seq = 0;
             uint8_t *body = frame.data + 1;
             while ((chunk_len = packetize(sbuf->data, data_len, body, CAN_MAX_DLEN - 1, &pos_buf))
-                   != 0) {
+                   != 0)
+            {
                 frame.data[0] = seq++;
                 frame.dlc = chunk_len + 1;
                 err = can_send(ts_can->dev, &frame,
